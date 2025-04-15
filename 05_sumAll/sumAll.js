661fc14116d0c3440c815bb7f82d0d1725d5b6ba
Gianni <132337749+giannindm@users.noreply.github.com>
2025-04-15 20:26:36 +0200

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((num1 < 0 || num2 < 0) || (!Number.isInteger(num1) || (!Number.isInteger(num2)))) {
+        return 'ERROR';
+    } else {
+        let smallest = num1;
+        let biggest = num2;
+        if (num1 > num2) {
+            smallest = num2;
+            biggest = num1;
+        }
+        let sum = 0;
+        for (let i = smallest; i <= biggest; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
